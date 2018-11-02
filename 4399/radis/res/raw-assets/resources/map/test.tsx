37b7f0062ba79156d63add68c22e31eb036b654e
Tishoy <chuang.han@mobvista.com>
2018-11-02 18:18:41 +0800

    4399box

--- 4399/radis/res/raw-assets/resources/map/test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="test" tilewidth="40" tileheight="40" tilecount="64" columns="8">
- <image source="tiled.png" width="320" height="320"/>
-</tileset>
