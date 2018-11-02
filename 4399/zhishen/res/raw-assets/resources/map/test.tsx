1c66b4f5413d39d5ae13d4d0b0277f91d28c50c3
Tishoy <chuang.han@mobvista.com>
2018-11-02 18:22:49 +0800

    4399

---
+++ 4399/zhishen/res/raw-assets/resources/map/test.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test" tilewidth="40" tileheight="40" tilecount="64" columns="8">
+ <image source="tiled.png" width="320" height="320"/>
+</tileset>
